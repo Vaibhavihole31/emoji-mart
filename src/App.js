2184ecaaed3029d94c649bc2ef0435eec4f55635
Vaibhavihole31 <itspinki05@gmail.com>
2022-12-07 18:05:33 +0530

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
+import Love from './img/love.png';
+import Crying from './img/crying.png';
+import Lol from './img/lol.png';
+import Heart from './img/heart.png';
+import Mealting from './img/melting.png';
 
 function App() {
+
+    const [emoji, setEmoji] = useState(<img src={Heart}/>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container'>
+     <div className='app-title-contanier'>
+     <h1 className='app-title'>🥳 Emoji Mart </h1>
+     </div>
+
+     <div className='main-emjio-contaner'>
+      <div className='main-emoji'>
+            <h3 className='emoji'>
+                {emoji}
+            </h3>
+      </div>
+     </div>
+
+        <div className='emoji-contanier'>
+          <div className='emoji-item' onClick={()=>{setEmoji(<img src={Heart}/>)}}><img className='item' src={Heart}/></div>
+          <div className='emoji-item' onClick={()=>{setEmoji(<img src={Crying}/>)}}><img className='item' src={Crying}/></div>
+          <div className='emoji-item' onClick={()=>{setEmoji(<img src={Lol}/>)}}><img className='item' src={Lol}/></div>
+          <div className='emoji-item' onClick={()=>{setEmoji(<img src={Mealting}/>)}}><img className='item' src={Mealting}/></div>
+          <div className='emoji-item' onClick={()=>{setEmoji(<img src={Love}/>)}}><img className='item' src={Love}/></div>
+        </div>
+
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
